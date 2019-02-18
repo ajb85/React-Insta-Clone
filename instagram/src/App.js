d6879702910c758bea9ef23b963330316414bf41
ajb85 <ajbrush85@gmail.com>
2019-02-18 17:31:14 -0500

    Search bar added, but not functional

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,14 +2,19 @@ import React, { Component } from "react";
 import "./App.css";
 import mockData from "./dummy-data.js";
 import PostContainer from "./components/PostContainer/PostContainer.js";
-//import SearchBar from "./components/SearchBar/SearchBar.js";
+import SearchBar from "./components/SearchBar/SearchBar.js";
 
 class App extends Component {
   render() {
     const posts = mockData.map((post, i) => (
       <PostContainer post={post} key={i} />
     ));
-    return <div>{posts}</div>;
+    return (
+      <div clasName="container">
+        <SearchBar />
+        <div className="postsContainer">{posts}</div>
+      </div>
+    );
   }
 }
 
---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./searchBar.css";
+
+export default function SearchBar(props) {
+  return (
+    <div className="searchBar">
+      <div className="logos">
+        <img src="https://i.imgur.com/RzOn2Qz.png" alt="IG logo" />
+      </div>
+      <div className="searchBox">
+        <input type="text" placeholder="Search" />
+      </div>
+      <div className="navIcons">
+        <i className="far fa-compass" />
+        <i className="far fa-heart" />
+        <i class="far fa-user" />
+      </div>
+    </div>
+  );
+}
