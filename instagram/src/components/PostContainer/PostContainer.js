07dff73d9bd4fb717ff5a99f5006f991ac834bf2
ajb85 <ajbrush85@gmail.com>
2019-02-18 16:13:33 -0500

    Post structure and style added

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -4,7 +4,20 @@ import Comments from "../CommentSection/CommentSection.js";
 // props: post
 export default function PostContainer(props) {
   return (
-    <div>
+    <div className="post">
+      <section className="postUser">
+        <img src={props.post.thumbnailUrl} />
+        <h2>{props.post.username}</h2>
+      </section>
+      <img src={props.post.imageUrl} />
+      <section className="postIcons">
+        <div>
+          <i class="far fa-heart" />
+          <i class="far fa-comment" />
+        </div>
+        <strong>{props.post.likes} likes</strong>
+      </section>
+
       <Comments comments={props.post.comments} />
     </div>
   );
